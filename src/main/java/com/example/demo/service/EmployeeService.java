ed0516b9a64df9dc229765c67b511929aed38e4f
venkadeshJ <venkadeshjpm@gmail.com>
2021-09-11 13:26:10 +0530

    springbootAplication

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Employee;
+
+
+public interface EmployeeService {
+	Employee saveEmployee(Employee employee);
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(long id);
+	Employee updateEmployee(Employee employee, long id);
+	void deleteEmployee(long id);
+}
